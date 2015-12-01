f760d7b692ac968c23c7264ddf3a9c7fa26c1683
Francisco Paniagua Reyes <FranciscoPR@MacBook-Pro-de-Francisco-2.local>
2015-12-01 17:48:40 -0600

    No JQuery

--- js/light_saber.js
+++ js/light_saber.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+
+
     var currentMousePos = {
 	x: -1,
 	y: -1
@@ -9,11 +10,19 @@ $(document).ready(function() {
 	x: 90,
 	y: 100
     };
+
     var audio = new Audio('audio/sthswng1.WAV');
-    $(".lightsaber").css("display", "none");
-    $(document).mousemove(function(event) {
-	//console.log("mousemove");
-	$(".lightsaber").css("display", "block");
+     var lightsaber =  document.createElement("IMG");
+     lightsaber.setAttribute("src","img/lightsaber.png");
+     lightsaber.setAttribute("class","lightsaber");
+     document.body.appendChild(lightsaber);	
+
+     lightsaber.style.display="none";
+     
+
+     document.onmousemove=function(event)
+     {
+    lightsaber.style.display="block";
 	var lastcurrentx = currentMousePos.x;
 	var lastcurrenty = currentMousePos.y;
 	currentMousePos.x = event.pageX;
@@ -22,33 +31,34 @@ $(document).ready(function() {
 	var rotationSpeed = Math.floor(Math.abs(lastcurrentx - currentMousePos.x));
 
 	if (rotation < 130) {
-	    if (rotationSpeed > 10){
-		audio.play();
+	    if (rotationSpeed > 10)
+	    {
+			audio.play();
 	    }
-	    if (lastcurrentx < currentMousePos.x) {
-		//if(rotation>0){
-		rotation += rotationSpeed;
-		menos.x = 10;
-		menos.y = 80;
-		//}
-	    }
+	    if (lastcurrentx < currentMousePos.x)
+	    {
+			rotation += rotationSpeed;
+			menos.x = 10;
+			menos.y = 80;
+		}
 	}
-	if (rotation > 0) {
-	    if (lastcurrentx > currentMousePos.x) {
-		rotation -= rotationSpeed;
-		menos.x = 90;
-		menos.y = 100;
-		//}
-	    }
+	if (rotation > 0) 
+	{
+	    if (lastcurrentx > currentMousePos.x) 
+	    {
+			rotation -= rotationSpeed;
+			menos.x = 90;
+			menos.y = 100;
+		}
 	}
-	$(".lightsaber").css({
-	    top: currentMousePos.y - menos.y,
-	    left: currentMousePos.x - menos.x,
-	    position: 'absolute'
-	});
-	if ((cont % 7) == 0) {
-	    $(".lightsaber").css("-webkit-transform", "rotate(" + rotation + "deg)");
-	    //cont=0;
-	}
-    });
-});
+
+		lightsaber.style.position="absolute";
+		console.log("x:"+currentMousePos.x+" -"+menos.x+" y:"+currentMousePos.y+" -"+menos.x);
+		lightsaber.style.top=(currentMousePos.y - menos.y)+"px" ;
+		lightsaber.style.left=(currentMousePos.x - menos.x)+"px";
+
+		if ((cont % 7) == 0) {
+		    lightsaber.style.webkitTransform="rotate(" + rotation + "deg)";
+		}
+
+    }
